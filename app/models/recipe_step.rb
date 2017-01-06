5c144b817436c5e36600b0e833b588703118a069
dydokamil <dydokamil@gmail.com>
2017-01-06 19:13:14 +0100

    Dodanie przepisu na naleśnik

--- app/models/recipe_step.rb
+++ app/models/recipe_step.rb
@@ -2,5 +2,5 @@ class RecipeStep < ApplicationRecord
   belongs_to :recipe
   has_many :steps_ingredients_joins
   has_many :ingredients, through: :steps_ingredients_joins
-  validates_uniqueness_of :number
+  # validates_uniqueness_of :number
 end
